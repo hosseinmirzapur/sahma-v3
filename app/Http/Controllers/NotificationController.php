cc54e2a39afb3fc58e87e9234cf961e8011c6cb9
hosseinmirzapur <hosseinmirzapur@gmail.com>
2025-02-10 01:57:25 +0330

    opt(controllers): NotificationController

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -5,68 +5,74 @@ namespace App\Http\Controllers;
 use App\Models\Letter;
 use App\Models\Notification;
 use App\Models\User;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Carbon;
-use Illuminate\Support\Facades\Validator;
 use Inertia\Response;
 use Inertia\ResponseFactory;
 
 class NotificationController extends Controller
 {
+    /**
+     * @param Request $request
+     * @return Response|ResponseFactory
+     */
     public function index(Request $request): Response|ResponseFactory
     {
-      /* @var User $user */
+        /* @var User $user */
         $user = $request->user();
+        if (!$user) {
+            abort(403, 'شما اجازه دسترسی به این قسمت را ندارید');
+        }
 
-        $notifications = $user->notifications()->get()->map(function (Notification $notification) {
-            return [
-            'letterId' => $notification->letter ? $notification->letter->id : null,
-            'subject' => $notification->subject,
-            'description' => $notification->description,
-            'remindAt' => $notification->remind_at,
-            'priority' => $notification->priority
-            ];
-        })->toArray();
+        $notifications = $user->notifications()
+            ->get()
+            ->map(function (Notification $notification) {
+                return [
+                    'letterId' => $notification->letter->id ?? null,
+                    'subject' => $notification->subject,
+                    'description' => $notification->description,
+                    'remindAt' => $notification->remind_at,
+                    'priority' => $notification->priority
+                ];
+            })->toArray();
 
         return inertia('Dashboard/Reminders/Reminder', [
-        'notifications' => $notifications
+            'notifications' => $notifications
         ]);
     }
 
-    public function createAction(Request $request)
+    /**
+     * @param Request $request
+     * @return RedirectResponse
+     */
+    public function createAction(Request $request): RedirectResponse
     {
-        /* @var User $user*/
+        /* @var User $user */
         $user = $request->user();
-        $request->validate([
-        'letter_id' => 'nullable|integer|exists:letters,id',
-        'subject' => 'required|string',
-        'remindAt' => 'required|string',
-        'description' => 'required|string',
-        'priority' => 'required|string|in:' . implode(',', Letter::getAllLetterPriorities())
-        ]);
-        $remindAt = $request->input('remindAt');
-        if (!is_null($remindAt)) {
-            $remindAt = Carbon::parse(strval($remindAt));
-        }
-        $letterId = $request->input('letter_id');
-        $letter = null;
-        if (!is_null($letterId)) {
-            $letter = Letter::find($letterId);
-        }
 
-        $subject = $request->input('subject');
-        $description = $request->input('description');
-        $priority = $request->input('priority');
+        $validatedData = $request->validate([
+            'letter_id' => 'nullable|integer|exists:letters,id',
+            'subject' => 'required|string',
+            'remindAt' => 'required|string',
+            'description' => 'required|string',
+            'priority' => 'required|string|in:' . implode(',', Letter::getAllLetterPriorities()),
+        ]);
+
+        $remindAt = Carbon::parse($validatedData['remindAt']);
+        $letterId = $validatedData['letter_id'] ?? null;
+        $letterId = Letter::query()->find($letterId)->id ?? null;
 
         Notification::query()->create([
-        'letter_id' => $letter ? $letter->id : null,
-        'user_id' => $user->id,
-        'subject' => $subject,
-        'remind_at' => $remindAt,
-        'description' => $description,
-        'priority' => $priority
+            'letter_id' => $letterId,
+            'user_id' => $user->id,
+            'subject' => $validatedData['subject'],
+            'remind_at' => $remindAt,
+            'description' => $validatedData['description'],
+            'priority' => $validatedData['priority'],
         ]);
 
         return redirect()->back();
     }
+
 }
