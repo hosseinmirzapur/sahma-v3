d2f08907e4ccf77a084378c7fe7737b7a4d59597
hosseinmirzapur <hosseinmirzapur@gmail.com>
2025-06-08 16:18:03 +0330

    feat: error handling on login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -17,83 +17,80 @@ use Inertia\ResponseFactory;
 
 class AuthController extends Controller
 {
-    public function __construct(private readonly ActivityService $activityService)
-    {
+  public function __construct(private readonly ActivityService $activityService)
+  {
+  }
+
+  public function index(): RedirectResponse
+  {
+    return redirect()->route('web.user.cartable.inbox.list');
+  }
+
+  public function loginPage(): Response|ResponseFactory
+  {
+    return inertia('Auth/Login');
+  }
+
+  /**
+   * @throws ValidationException
+   * @throws Exception
+   */
+  public function loginAction(Request $request): RedirectResponse
+  {
+    $validated = $request->validate([
+      'username' => 'required|string',
+      'password' => 'required|string|max:72'
+    ]);
+
+    $user = User::query()
+      ->where('personal_id', $validated['username'])
+      ->first();
+
+    if (!$user || $user->deleted_at !== null || !Hash::check(strval($validated['password']), strval($user->password))) {
+      throw ValidationException::withMessages(['username' => 'نام کاربری یا رمز عبور اشتباه است.']);
     }
 
-    public function index(): RedirectResponse
-    {
-        return redirect()->route('web.user.cartable.inbox.list');
+    // Rehash password if needed (more secure than manually checking $argon2id)
+    if (password_needs_rehash(strval($user->password), PASSWORD_ARGON2ID)) {
+      $user->update(['password' => Hash::make($validated['password'])]);
     }
 
-    public function loginPage(): Response|ResponseFactory
-    {
-        return inertia('Auth/Login');
+    $this->activityService->logUserAction(
+      $user,
+      Activity::TYPE_LOGIN,
+      $user,
+      "کاربر $user->name وارد سیستم شد."
+    );
+
+    Auth::guard('web')->login($user);
+    if (config('ui.has_cartable')) {
+      return redirect()->route('web.user.cartable.inbox.list');
     }
 
-    /**
-     * @throws ValidationException
-     * @throws Exception
-     */
-    public function loginAction(Request $request): RedirectResponse
-    {
-        $validated = $request->validate([
-            'username' => 'required|string',
-            'password' => 'required|string|max:72'
-        ]);
+    return redirect()->route('web.user.dashboard.index');
+  }
 
-        $user = User::query()
-            ->where('personal_id', $validated['username'])
-            ->first();
+  public function logout(Request $request, ActivityService $activityService): RedirectResponse
+  {
+    /** @var User $user */
+    $user = $request->user();
 
-        abort_if(!$user, 422, 'کاربری با این شناسه وجود ندارد.');
-        abort_if($user->deleted_at !== null, 403, 'مجوز ورود به سیستم را ندارید.');
+    Redis::connection('cache')
+      ->client()
+      ->hset(
+        'logged-out-users-today',
+        (string) $user->id,
+        (string) now()->timestamp
+      );
 
-        if (!Hash::check(strval($validated['password']), strval($user->password))) {
-            throw ValidationException::withMessages(['message' => 'رمز عبور اشتباه است.']);
-        }
+    $activityService->logUserAction(
+      $user,
+      Activity::TYPE_LOGOUT,
+      $user,
+      "کاربر $user->name از سیستم خارج شد."
+    );
 
-        // Rehash password if needed (more secure than manually checking $argon2id)
-        if (password_needs_rehash(strval($user->password), PASSWORD_ARGON2ID)) {
-            $user->update(['password' => Hash::make($validated['password'])]);
-        }
-
-        $this->activityService->logUserAction(
-            $user,
-            Activity::TYPE_LOGIN,
-            $user,
-            "کاربر $user->name وارد سیستم شد."
-        );
-
-        Auth::guard('web')->login($user);
-        if (config('ui.has_cartable')) {
-            return redirect()->route('web.user.cartable.inbox.list');
-        }
-
-        return redirect()->route('web.user.dashboard.index');
-    }
-
-    public function logout(Request $request, ActivityService $activityService): RedirectResponse
-    {
-        /** @var User $user */
-        $user = $request->user();
-
-        Redis::connection('cache')
-            ->client()
-            ->hset(
-                'logged-out-users-today',
-                (string)$user->id,
-                (string)now()->timestamp
-            );
-
-        $activityService->logUserAction(
-            $user,
-            Activity::TYPE_LOGOUT,
-            $user,
-            "کاربر $user->name از سیستم خارج شد."
-        );
-
-        Auth::guard('web')->logout();
-        return redirect()->route('web.user.login-page');
-    }
+    Auth::guard('web')->logout();
+    return redirect()->route('web.user.login-page');
+  }
 }
--- resources/vue/Pages/Auth/Login.vue
+++ resources/vue/Pages/Auth/Login.vue
@@ -1,17 +1,13 @@
 <template>
   <div>
-    <p
-      v-for="(err, index) in v$.$errors"
-      :key="index"
-      v-text="err" />
     <form @submit.prevent="submit">
       <div class="flex justify-between items-center w-full p-4">
         <ul>
-          <!-- eslint-disable vue/no-v-html -->
           <li
+            v-if="form.errors.username"
             class="text-right ml-3 text-xs md:text-sm font-medium text-red-500"
-            v-html="error" />
-          <!-- eslint-enable vue/no-v-html -->
+            v-text="form.errors.username"
+          />
         </ul>
       </div>
       <AuthInput
@@ -19,7 +15,8 @@
         input-label="نام کاربری"
         input-name="username"
         input-type="text"
-        maxlength="30" />
+        maxlength="30"
+      />
 
       <AuthInput
         v-model="form.password"
@@ -28,13 +25,15 @@
         input-type="password"
         :is-show="isShow"
         maxlength="72"
-        @toggle="isShow=!isShow" />
+        @toggle="isShow = !isShow"
+      />
 
       <button
         :disabled="isLoading"
         type="submit"
-        class="bg-primary rounded-full w-[100%] h-11  text-white text-lg  hover:opacity-90 mt-8"
-        :class="(isLoading===true)? '!opacity-40 cursor-not-allowed' : ''">
+        class="bg-primary rounded-full w-[100%] h-11 text-white text-lg hover:opacity-90 mt-8"
+        :class="isLoading === true ? '!opacity-40 cursor-not-allowed' : ''"
+      >
         ادامه
       </button>
     </form>
@@ -42,61 +41,51 @@
 </template>
 
 <script setup>
-import layout from '../../Layouts/~AppLayoutAuth.vue'
-import { ref } from 'vue'
-import { useForm } from '@inertiajs/inertia-vue3'
-import AuthInput from '../../Pages/Components/AuthInput.vue'
-import { useVuelidate } from '@vuelidate/core'
-import { required } from '@vuelidate/validators'
+import layout from "../../Layouts/~AppLayoutAuth.vue";
+import { ref } from "vue";
+import { useForm } from "@inertiajs/inertia-vue3";
+import AuthInput from "../../Pages/Components/AuthInput.vue";
+import { useVuelidate } from "@vuelidate/core";
+import { required } from "@vuelidate/validators";
 
 // eslint-disable-next-line no-undef
 defineOptions({
-  name: 'Login',
-  layout
-})
-const error = ref('')
-const isLoading = ref(false)
-const isShow = ref(true)
+  name: "Login",
+  layout,
+});
+const isLoading = ref(false);
+const isShow = ref(true);
 const form = useForm({
-  username: '',
-  password: ''
-})
+  username: "",
+  password: "",
+});
 
 const rules = {
   username: { required },
-  password: { required }
+  password: { required },
+};
+
+const v$ = useVuelidate(rules, form);
+
+function restart() {
+  form.username = "";
+  form.password = "";
 }
 
-const v$ = useVuelidate(rules, form)
-
-function restart () {
-  form.username = ''
-  form.password = ''
-}
-
-function submit () {
+function submit() {
   if (!v$.value.$pending && !v$.value.$invalid) {
     // eslint-disable-next-line no-undef
-    form.post(route('web.user.login'), {
+    form.post(route("web.user.login"), {
       replace: true,
       preserveScroll: true,
       onStart: () => {
-        isLoading.value = true
-      },
-      onError: (e) => {
-        error.value = ''
-        for (const arrError in e) {
-          if (arrError in e) {
-            error.value += `${e[arrError]} <br/> `
-          }
-        }
+        isLoading.value = true;
       },
       onFinish: () => {
-        isLoading.value = false
-        restart()
-      }
-    })
+        isLoading.value = false;
+        restart();
+      },
+    });
   }
 }
-
 </script>
